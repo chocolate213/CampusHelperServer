a251a5ad83668efa1c04f5d5122b56ccb1d4f46f
Jiaxing Zhang <chocolatepie213@gmail.com>
2018-07-04 15:29:05 +0800

    add test code line 3

--- src/main/java/cn/jxzhang/common/message/RequestMessage.java
+++ src/main/java/cn/jxzhang/common/message/RequestMessage.java
@@ -12,5 +12,6 @@ public class RequestMessage {
     public static void main(String[] args) {
         System.out.println("add code");
         System.out.println("add code 2");
+        System.out.println("add code 3");
     }
 }
